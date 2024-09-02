7fc39376c40b5242c9c17918c169b8aa4ce2cb0f
dinhtanloc <locdinh.31221020226@st.ueh.edu.vn>
2024-09-02 12:55:12 +0700

    design frontend

--- src/frontend/src/routers/Router.jsx
+++ src/frontend/src/routers/Router.jsx
@@ -1,28 +1,33 @@
 import { useState, useEffect } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Dashboard from "@pages/Dashboardpage";
+import HomePage from "../pages/HomePage";
 
 const Routers = () => {
   const [isLoading, setIsLoading] = useState(true);
-    const location = useLocation();
+  const location = useLocation();
 
-      useEffect(() => {
-            setIsLoading(true);
-        
-            const timer = setTimeout(() => {
-              setIsLoading(false);
-            }, 1000); // Giả lập thời gian tải trang
-        
-            return () => clearTimeout(timer);
-          }, [location]);
-        
-          if (isLoading) {
-            return <LoadingPage />;
-          }
+  useEffect(() => {
+        setIsLoading(true);
+    
+        const timer = setTimeout(() => {
+          setIsLoading(false);
+        }, 1000); // Giả lập thời gian tải trang
+    
+        return () => clearTimeout(timer);
+      }, [location]);
+    
+  if (isLoading) {
+    return <LoadingPage />;
+  }
     return (
       <Routes>
               <Route path="/" element={<Navigate to="/home" />} />
-              <Route path="/home" element={<Dashboard />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/register" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              
               
       </Routes>
     );
