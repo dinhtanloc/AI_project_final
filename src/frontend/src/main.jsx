2e78d8f4254070e723b34a8bfad31b0b84a62ec2
Dinh Tan Loc <dinhtanloc.dynamicueh@gmail.com>
2024-09-06 18:28:02 +0700

    test .env

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -11,7 +11,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App.jsx'
 import './index.css'
-
+console.log(import.meta.env.VITE_AG_CHART)
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   //   <App />
