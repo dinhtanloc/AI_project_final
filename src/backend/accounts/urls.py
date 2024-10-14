6c9343cc754ea213d1a343c32fad6ff7d092df14
Dinh Tan Loc <dinhtanloc.dynamicueh@gmail.com>
2024-10-15 00:27:47 +0700

    change code

--- src/backend/accounts/urls.py
+++ src/backend/accounts/urls.py
@@ -5,11 +5,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+app_name='accounts'
 router = DefaultRouter()
 router.register(r'user', views.UserViewSet, basename='user')
 
 
-app_name='accounts'
 urlpatterns = [
  	path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
